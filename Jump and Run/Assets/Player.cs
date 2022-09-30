73a77ddd7ed36b347157ed845a07f5aaf3728dbc
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2022-09-30 07:39:51 +0200

    ^^
    ^^

---
+++ Jump and Run/Assets/Player.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumph = 7;
+    private bool isgrounded = false;
+    private Rigidbody2D rb;
+    private Vector3 rotation;
+    private CoinManager m;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rotation = transform.eulerAngles;
+        m = GameObject.FindGameObjectWithTag("Text").GetComponent<CoinManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float richtung = Input.GetAxis("Horizontal");
+        if(richtung < 0)
+        {
+            transform.eulerAngles = rotation - new Vector3(0, 180, 0);
+            transform.Translate(Vector2.right * speed * -richtung * Time.deltaTime);
+        }
+        if(richtung > 0)
+        {
+            transform.eulerAngles = rotation;
+            transform.Translate(Vector2.right * speed * richtung * Time.deltaTime);
+        }
+        
+
+        if(Input.GetKeyDown(KeyCode.Space) && isgrounded)
+        {
+            rb.AddForce(Vector2.up * jumph, ForceMode2D.Impulse);
+            isgrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "ground")
+        {
+            isgrounded = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            m.Addmoney();
+            Destroy(other.gameObject);
+        }
+    }
+}
