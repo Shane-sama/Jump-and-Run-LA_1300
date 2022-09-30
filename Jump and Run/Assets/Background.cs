561ffefa90b77bda0eec9ca4377a9d54d67f7219
Marmeladenbrot1 <N.goehl.inf21@stud.bbbaden.ch>
2022-09-30 13:10:20 +0200

    Finish for now i think

--- Jump and Run/Assets/Background.cs
+++ Jump and Run/Assets/Background.cs
@@ -12,20 +12,49 @@ public class Background : MonoBehaviour
     float[] backSpeed;
     [Range(0.01f, 0.6f)]
     public float parallaxSpeed;
+    float farthestBack;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main.transform;
-        camStartPosm = cam.position;
+        camStartPos = cam.position;
         int BackCount = transform.childCount;
         mat = new Material[BackCount];
         backSpeed = new float[BackCount];
+        backgrounds = new GameObject[BackCount];
 
+        for(int i = 0; i < BackCount; i++)
+        {
+            backgrounds[i] = transform.GetChild(i).gameObject;
+            mat[i] = backgrounds[i].GetComponent<Renderer>().material;
+        }
+        backSpeedCalculate(BackCount);
     }
 
-    // Update is called once per frame
-    void Update()
+    void backSpeedCalculate(int BackCount)
     {
-        
+        for(int i = 0; i < BackCount; i++)
+        {
+            if((backgrounds[i].transform.position.z - cam.position.z) > farthestBack)
+            {
+                farthestBack = backgrounds[i].transform.position.z - cam.position.z;
+            }
+        }
+
+        for(int i = 0;i < BackCount; i++)
+        {
+            backSpeed[i] = 1 - (backgrounds[i].transform.position.z - cam.position.z) / farthestBack;
+        }
+    }
+    private void LateUpdate()
+    {
+        distance = cam.position.x - camStartPos.x;
+        transform.position = new Vector3(cam.position.x, transform.position.y, 0);
+
+        for(int i = 0; i < backgrounds.Length; i++)
+        {
+            float speed = backSpeed[i] * parallaxSpeed;
+            mat[i].SetTextureOffset("_MainTex", new Vector2(distance, 0) * speed);
+        }
     }
 }
