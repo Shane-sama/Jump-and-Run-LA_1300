befdf43b980d2edf58fe03f1317b8e52e2562551
Marmeladenbrot1 <89131184+Marmeladenbrot1@users.noreply.github.com>
2022-10-03 12:56:01 +0200

    Test
    Test

--- Jump and Run/Assets/Buttons.cs
+++ Jump and Run/Assets/Buttons.cs
@@ -18,6 +18,6 @@ public class Buttons : MonoBehaviour
     }
     public void Restart()
     {
-        SceneManager.LoadScene("MainScene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
